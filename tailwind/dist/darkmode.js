2a926638fd2ddc150cfc9b21170966808d18de26
AnoukvanGemert <anouk.v.gemert@gmail.com>
2024-09-06 12:09:02 +0200

    git add all

--- tailwind/dist/darkmode.js
+++ tailwind/dist/darkmode.js
@@ -1,34 +1,49 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const themeSwitch = document.getElementById('theme-switch');
+let darkmode = localStorage.getItem('dark');
+const themeSwitch = document.getElementById('theme-switch');
 
-  if (themeSwitch) {
-      let darkmode = localStorage.getItem('darkmode');
+const enableDarkmode = () => {
+    document.body.classList.add('dark');
+    localStorage.setItem('darkmode', 'active')
+}
 
-      const enableDarkmode = () => {
-          document.body.classList.add('dark');
-          localStorage.setItem('darkmode', 'active');
-      }
+const disableDarkmode = () => {
+    document.body.classList.remove('dark');
+    localStorage.setItem('darkmode', 'inactive')
+}
 
-      const disableDarkmode = () => {
-          document.body.classList.remove('dark');
-          localStorage.setItem('darkmode', 'inactive');
-      }
+if(darkmode === "active") enableDarkmode();
 
-      if (darkmode === "active") {
-          enableDarkmode();
-      }
-
-      themeSwitch.addEventListener("click", () => {
-          darkmode = localStorage.getItem('darkmode');
-          if (darkmode !== "active") {
-              enableDarkmode();
-          } else {
-              disableDarkmode();
-          }
-      });
-  } else {
-      console.error('Theme switch button not found!');
-  }
+themeSwitch.addEventListener("click" , () => {
+    darkmode = localStorage.getItem('darkmode');
+    if(darkmode !== "active") {
+        enableDarkmode();
+    }
+    else {
+        disableDarkmode();
+    }
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+    const detailsElement = document.querySelector("details");
+  
+    const closeButton = document.querySelector(".bg-gray-400");
+  
+    closeButton.addEventListener("click", function () {
+      detailsElement.removeAttribute("open");
+    });
+  
+    
+    detailsElement.addEventListener("toggle", function () {
+      if (detailsElement.open) {
+        console.log("Details opened");
+      } else {
+        console.log("Details closed");
+      }
+    });
+  });
+
+  document.querySelector('.popup-overlay').addEventListener('click', function () {
+    document.querySelector('details').removeAttribute('open');
+  });
+  
   
